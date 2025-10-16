4f60e5104a90e89c98c19f35239d47a9453cde22
Javid Baku <cavid-de-baku91@mail.ru>
2025-10-16 10:01:15 +0400

    new interview

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,9 @@ import hecer3 from '../photos/hajar12.png';
 import hecer4 from '../photos/titles/home4.jpg';
 import hecer5 from '../photos/hajar11.png';
 import hecer8 from '../photos/hajar1.jpg';
+import hecer9 from '../photos/hecer9.webp';
 import fransa from '../photos/titles/fransa.png';
+
 import { useTranslation } from 'react-i18next';
 
 
@@ -51,6 +53,7 @@ const interviews = [
   { name: '“Erməni-qriqoryan kilsəsi XIX əsr ərzində alban irsini məhv edib, qarət edirdi”', url: 'https://1905.az/erm%C9%99ni-qriqoryan-kils%C9%99si-xix-%C9%99sr-%C9%99rzind%C9%99-alban-irsini-m%C9%99hv-edib-qar%C9%99t-edirdi/', image: hecer2 },
   { name: 'Развязывая «узлы» истории', url: 'https://br.az/politics/63227/razvyazyvaya-uzly-istorii/', image: hecer4 },
   { name: 'Архитектура вне времени', url: 'https://baki-baku.az/%D0%B0%D1%80%D1%85%D0%B8%D1%82%D0%B5%D0%BA%D1%82%D1%83%D1%80%D0%B0-%D0%B2%D0%BD%D0%B5-%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8/?fbclid=IwZXh0bgNhZW0CMTEAAR7T2upPLZLKcAh2Xop5Jp_vUT4IdCPoqCrUoCMSxmyQI-tL9OgETARdpQ3WqA_aem_RCNz06CnFG1sO8bN70pjMg', image: hecer7 },
+  { name: 'Restoring Stolen Truth: Caucasian Albania vs. Centuries of Historical Falsification', url: 'https://caspianpost.com/stories/restoring-stolen-truth-caucasian-albania-vs-centuries-of-historical-falsification', image: hecer9 },
   { name: 'История, искаженная догмой', url: 'https://br.az/politics/79452/istoriya-iskazhennaya-dogmoy/https://br.az/politics/79452/istoriya-iskazhennaya-dogmoy/?fbclid=IwY2xjawKArJxleHRuA2FlbQIxMQBicmlkETEyaWVUSGplY29NS1NsODdXAR66nWfEw_q5AJIrxs6aB0_HBQF3I6ouU9oW2XsDf0selhVkdrQdUBdGT7lGhA_aem_okc8tzfp0txYI818OwKgHg', image: hecer8 }
 ];
 
